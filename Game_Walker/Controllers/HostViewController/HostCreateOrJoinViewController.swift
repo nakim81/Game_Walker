f4b90c6d41129f1450e0b198fdb2aef9eaad4e59
jinkim2739 <67286328+jinkim2739@users.noreply.github.com>
2022-08-17 21:22:46 -0700

    before pull 0817

--- Game_Walker/Controllers/HostViewController/HostCreateOrJoinViewController.swift
+++ Game_Walker/Controllers/HostViewController/HostCreateOrJoinViewController.swift
@@ -11,7 +11,6 @@ class HostCreateOrJoinViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
@@ -20,11 +19,11 @@ class HostCreateOrJoinViewController: BaseViewController {
     
 
     @IBAction func createButtonPressed(_ sender: UIButton) {
-        K.gamecode = String(Int.random(in: 100000 ... 999999))
-        let host = Host(gamecode: K.gamecode)
+        var gc = String(Int.random(in: 100000 ... 999999))
+        let host = Host2(gamecode: K.gamecode)
         print(K.gamecode)
-        K.Database.updateHost(host)
-        K.Database.readHost(gamecode: K.gamecode, onListenerUpdate: listen(_:))
+        K.Database.createGame(gc, host)
+        
     }
     
 
