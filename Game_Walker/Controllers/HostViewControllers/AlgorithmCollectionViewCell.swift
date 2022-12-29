05adcba1e9f49e42122b7ab05efcaa9461ee7843
jinkim2739 <67286328+jinkim2739@users.noreply.github.com>
2022-12-29 05:15:37 -0800

    aggagad basic cell idk

---
+++ Game_Walker/Controllers/HostViewControllers/AlgorithmCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AlgorithmCollectionViewCell.swift
+//  Game_Walker
+//
+//  Created by Jin Kim on 12/29/22.
+//
+
+import UIKit
+
+class AlgorithmCollectionViewCell: UICollectionViewCell {
+    
+    func configureAlgorithmCell(imageName: String) {
+        
+    }
+}
