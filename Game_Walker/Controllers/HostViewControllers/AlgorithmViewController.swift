9b031553ce10d68410dd5e5852085f1570f29b4e
jinkim2739 <67286328+jinkim2739@users.noreply.github.com>
2022-12-30 00:47:32 -0800

    trying to make grid

--- Game_Walker/Controllers/HostViewControllers/AlgorithmViewController.swift
+++ Game_Walker/Controllers/HostViewControllers/AlgorithmViewController.swift
@@ -11,9 +11,10 @@ class AlgorithmViewController: BaseViewController {
     
     var curr_gamecode = String(data: UserDefaults.standard.data(forKey: "gamecodestring")!, encoding: .utf8)!
     var stationList: [Station] = []
-    var teamList: [Team] = []
+//    var teamList: [Team] = []
     var host: Host?
-    var teamnums :[Int] = []
+//    var teamnums :[Int] = []
+    var grid: [[Int]] = []
 
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -24,8 +25,8 @@ class AlgorithmViewController: BaseViewController {
         
         S.delegate_stationList = self
         S.getStationList(curr_gamecode)
-        T.delegate_teamList = self
-        T.getTeamList(curr_gamecode)
+//        T.delegate_teamList = self
+//        T.getTeamList(curr_gamecode)
         H.delegate_getHost = self
         collectionView.delegate = self
         collectionView.dataSource = self
@@ -53,8 +54,14 @@ extension AlgorithmViewController: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlgorithmCollectionViewCell", for: indexPath) as? AlgorithmCollectionViewCell else { return UICollectionViewCell() }
 
-        let num_team = teamList[indexPath.item].number
-        
+//        let num_team = teamList[indexPath.item].number
+        let num_team = host!.teams
+        var num_cols = stationList.count
+        var num_rows = host!.rounds
+        if (num_team < 8) {
+            num_cols = 8
+            num_rows = 8
+        }
         cell.configureAlgorithmCell(teamIndex: <#T##Int#>, teamnums: teamnums)
         return cell
     }
