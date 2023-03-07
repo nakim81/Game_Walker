973f154c8f6419497a2a07148ed1cea82ed61928
jinkim2739 <67286328+jinkim2739@users.noreply.github.com>
2023-03-06 22:16:53 -0800

    did nothing just commit before push

--- Game_Walker/Controllers/HostViewControllers/HostCreateOrJoinViewController.swift
+++ Game_Walker/Controllers/HostViewControllers/HostCreateOrJoinViewController.swift
@@ -21,6 +21,11 @@ class HostCreateOrJoinViewController: BaseViewController {
 //        let host = Host(gamecode: "705154")
         let host = Host(gamecode: gc)
         H.createGame(gc, host)
+        
+//        let host1 = Host(gamecode: "123456")
+//        let host2 = Host(gamecode: "999999")
+//        H.createGame("123456", host1)
+//        H.createGame("999999", host2)
 //        UserData.writeGamecode("705154", "gamecode")
         UserData.writeGamecode(gc, "gamecode")
 //        T.listenTeams("705154", onListenerUpdate: listen(_:))
