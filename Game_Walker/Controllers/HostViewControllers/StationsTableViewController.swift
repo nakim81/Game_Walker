58a18295257ef9b0217a68b901d6dd0eb8f8d6b9
jinkim2739 <67286328+jinkim2739@users.noreply.github.com>
2023-03-13 21:39:32 -0700

    testing my modal segue for stations

--- Game_Walker/Controllers/HostViewControllers/StationsTableViewController.swift
+++ Game_Walker/Controllers/HostViewControllers/StationsTableViewController.swift
@@ -50,6 +50,12 @@ extension StationsTableViewController: UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "AddStationViewController") as? AddStationViewController {
+            self.navigationController?.popToViewController(vc, animated: true)
+        }
+    }
+    
     
 }
 
